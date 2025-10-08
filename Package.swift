a2992cd555bb36dfea82956395895276695ce6d4
Tom Stovall <119924+stovak@users.noreply.github.com>
2025-10-08 10:40:24 -0700

    remove fixtures

--- Package.swift
+++ Package.swift
@@ -1,11 +1,9 @@
-// swift-tools-version:5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:6.2
 import PackageDescription
 
 let package = Package(
   name: "SwiftFFmpeg",
-  platforms: [.macOS(.v10_15)],
+  platforms: [.macOS("26")],
   products: [
     .library(
       name: "SwiftFFmpeg",
@@ -13,6 +11,18 @@ let package = Package(
     )
   ],
   targets: [
+    .plugin(  name: "FetchFFmpegXCFrameworks",
+              capability: .command(
+                intent: .custom(
+                    verb: "download-ffmpeg-xcframeworks",
+                    description: "Download the latest stovak/ffmpeg-framework XCFramework artifact into the package."
+                    ),
+                    permissions: [
+                      .writeToPackageDirectory(reason: "Place the downloaded FFmpeg XCFrameworks under the xcframework/ directory.")
+                    ]
+                )
+            ),
+
     .systemLibrary(
       name: "CFFmpeg",
       pkgConfig: "libavformat",
